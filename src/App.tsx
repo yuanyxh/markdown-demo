eab7baffda65f9609f19e868d2ea24b71ef29b39
yuanyxh <15766118362@139.com>
2024-11-27 11:21:48 +0800

    feat: change

--- src/App.tsx
+++ src/App.tsx
@@ -12,15 +12,15 @@ import {
 import type {
   AttributeProvider,
   AttributeProviderFactory,
-  Node,
+  MarkdownNode,
 } from "./packages/commonmark";
 import { AttributeProviderContext } from "./packages/commonmark/renderer";
 
-const nodeMap = new Map<string, Node>();
+const nodeMap = new Map<string, MarkdownNode>();
 let nextId = 1;
 class NodeMapAttributeProvider implements AttributeProvider {
   setAttributes(
-    node: Node,
+    node: MarkdownNode,
     tagName: string,
     attributes: Map<string, string>
   ): void {
@@ -45,6 +45,7 @@ const htmlRenderer = HtmlRenderer.builder()
   .attributeProviderFactory(new NodeMapAttributeProviderFactory())
   .build();
 
+/** 重渲染 */
 function reRender(markdownText: string) {
   const document = parser.parse(markdownText);
 
@@ -55,96 +56,21 @@ function reRender(markdownText: string) {
   }
 }
 
+function getMarkdownNodes(sC: Node, eC: Node) {}
+
 const onBeforeInput = (e: InputEvent) => {
   e.preventDefault();
+
+  // the insertText process
   const ranges = e.getTargetRanges();
 
   const [range] = ranges;
-
-  let start = 0;
-  let end = 0;
-  if (range.startContainer instanceof Text) {
-    const startParent = range.startContainer.parentElement;
-    const id = startParent?.dataset.id;
-
-    const textIndex = Array.from(startParent?.childNodes || []).findIndex(
-      (ele) => ele === range.startContainer
-    );
-
-    if (id) {
-      const startNodeParent = nodeMap.get(id);
-
-      let i = 1;
-      let textNode = startNodeParent?.getFirstChild();
-
-      while (i < textIndex && textNode) {
-        i++;
-
-        textNode = textNode.getNext();
-      }
-
-      if (textNode) {
-        const span = textNode.getSourceSpans()[0];
-        start = (span?.getInputIndex() || 0) + range.startOffset;
-      }
-    }
-  }
-
-  if (range.endContainer instanceof Text) {
-    const endParent = range.endContainer.parentElement;
-    const id = endParent?.dataset.id;
-
-    const textIndex = Array.from(endParent?.childNodes || []).findIndex(
-      (ele) => ele === range.endContainer
-    );
-
-    if (id) {
-      const endNodeParent = nodeMap.get(id);
-
-      let i = 1;
-      let textNode = endNodeParent?.getFirstChild();
-
-      while (i < textIndex && textNode) {
-        i++;
-
-        textNode = textNode.getNext();
-      }
-
-      if (textNode) {
-        const span = textNode.getSourceSpans()[0];
-        end = (span?.getInputIndex() || 0) + range.endOffset;
-      }
-    }
-  }
-
-  console.log(e.inputType);
-  console.log(range);
-  console.log(start, end);
-
-  if (e.inputType === "insertText") {
-    markdownText =
-      markdownText.slice(0, start) + e.data + markdownText.slice(end);
-
-    const selection = window.document.getSelection();
-    let savedRange!: Range;
-    if (selection && selection.rangeCount > 0) {
-      savedRange = selection.getRangeAt(0);
-      console.log(savedRange.startContainer, savedRange.startOffset);
-      console.log(savedRange.endContainer, savedRange.endOffset);
-    }
-
-    reRender(markdownText);
-
-    if (savedRange) {
-      console.log(savedRange);
-      const newSelection = window.document.getSelection();
-      const newRange = document.createRange();
-      newRange.setStart(savedRange.startContainer, savedRange.startOffset);
-      newRange.setEnd(savedRange.endContainer, savedRange.endOffset);
-      newSelection?.removeAllRanges();
-      newSelection?.addRange(newRange);
-    }
-  }
+  const {
+    startContainer: sC,
+    startOffset: sF,
+    endContainer: eC,
+    endOffset: eF,
+  } = range;
 };
 
 const parser = Parser.builder()
