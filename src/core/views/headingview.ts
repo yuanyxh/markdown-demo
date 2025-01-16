9e282095f2af041dd3c96af913ea85749bd13b6d
yuanyxh <15766118362@139.com>
2025-01-16 17:56:51 +0800

    feat: heading eq override

--- src/core/views/headingview.ts
+++ src/core/views/headingview.ts
@@ -1,4 +1,4 @@
-import type { Heading } from 'commonmark-java-js';
+import type { Heading, MarkdownNode } from 'commonmark-java-js';
 
 import type InlineView from './abstracts/inlineview';
 
@@ -7,6 +7,17 @@ import BlockView from './abstracts/blockview';
 class HeadingView extends BlockView {
   public length: number = 0;
   public children: InlineView[] = [];
+  public node: Heading;
+
+  public constructor(node: Heading) {
+    super(node);
+
+    this.node = node;
+  }
+
+  public override eq(node: Heading): boolean {
+    return node.getLevel() === this.node.getLevel();
+  }
 
   protected createElement(node: Heading): HTMLElement {
     return window.document.createElement('h' + node.getLevel());
